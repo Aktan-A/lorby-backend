fe334f606893de8b92dce06636e47545d7f7772c
Aktan A <aktan.amanaliev@gmail.com>
2024-07-03 13:24:43 +0600

    Add email verification endpoint to swagger

--- src/main/java/com/neobis/lorby/controller/EmailController.java
+++ src/main/java/com/neobis/lorby/controller/EmailController.java
@@ -1,6 +1,9 @@
 package com.neobis.lorby.controller;
 
 import com.neobis.lorby.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,13 @@ public class EmailController {
 
     private final EmailService emailService;
 
+    @Operation(
+            summary = "Confirm email",
+            description = "Verifies the users email address using the token which was sent during registration")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Email successfully confirmed"),
+            @ApiResponse(responseCode = "400", description = "Email confirmation token not found")
+    })
     @GetMapping("/confirm/{token}")
     public ResponseEntity<String> confirmEmail(@PathVariable("token") String token) {
         emailService.confirmEmailByToken(token);
